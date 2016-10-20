8deeec8be4e4d25e37f4e596bc5d3b40c4041eb5
ciberglubsch <berndt.brod@gmx.de>
2016-10-20 12:38:55 +0200

    Finished integration

--- compiler/test.asm
+++ compiler/test.asm
@@ -1,11 +1,8 @@
-ADDI x1, x0, 0x7FF
-ADDI x1, x1, 0x001
+LUI x1, 0x10000
 LUI x10, 0xFFFFF
 ADDI x10, x10, 0xFFF
 LUI x11, 0xAAAAA
 ADDI x11, x11, 0xAAA
-LB x9, x0, 0x000
-LW x12, x0, 0x000
 LW x4, x1, 0x000
 LW x5, x1, 0x008
 LW x6, x1, 0x000
@@ -13,3 +10,12 @@ SW x1, x10, 0x000
 SW x1, x11, 0x008
 LW x7, x1, 0x000
 LW x8, x1, 0x008
+LUI x12, 0x80000
+SRAI x13, x12, 0x1
+SRLI x14, x13, 0x1
+SLLI x15, x14, 0x1
+ADDI x16, x0, 0x003
+LUI x17, 0x80000
+SRA x18, x17, x16
+SRL x19, x18, x16
+SLL x20, x19, x16
