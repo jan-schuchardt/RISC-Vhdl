f2487ea9e02036a9344c6fd05707f64a18b6b350
Wodka <domi.erdnuss2@gmx.de>
2016-11-10 15:37:02 +0100

    Fixed compiler to 32 bit length (again)

--- compiler/riscv_instr_format.py
+++ compiler/riscv_instr_format.py
@@ -61,3 +61,18 @@ def sb_imm_split(imm):
 	imm2 = (rshift(imm, 5) & 0x3F)
 	imm2 = imm2 | ((rshift(imm, 12) & 1)<<6)
 	return (int(imm1), int(imm2))
+	
+def jal_imm_split(imm):
+	print(imm)
+	imm_o = int(imm) #original immediate value
+	bit_19_to_12 = rshift(imm_o & 0xFFFFF, 12)
+	imm = compile_bitfield(0, 8, bit_19_to_12, 0)
+	bit_11 = rshift(imm_o & 0x800, 11)
+	imm = compile_bitfield(8, 9, bit_11, imm)
+	bit_10_to_1 = rshift(imm_o & 0x7FF, 1)
+	imm = compile_bitfield(9, 19, bit_10_to_1, imm)
+	bit_20 = rshift(imm_o & 0x100000, 20)
+	print(bit_20)
+	imm = compile_bitfield(19, 20, bit_20, imm)
+	print(imm)
+	return imm
