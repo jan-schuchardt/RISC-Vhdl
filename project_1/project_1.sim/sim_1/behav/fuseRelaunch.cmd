84f47471c48453d0bf8380417aa9a7674a966bb9
era-gp-member <ga58taw@mytum.de>
2017-02-09 17:19:52 +0100

    Cleaned up.

--- project_1/project_1.sim/sim_1/behav/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "pa" -incremental --nodebug -L "work" -o "toplevel.exe" --prj "C:/Users/riege/gpgit/RISC-Vhdl/RISC-Vhdl/project_1/project_1.sim/sim_1/behav/toplevel.prj" -top "work.toplevel" 
